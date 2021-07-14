7cf7d1951689885f661dc96b234b66ed299bd6c2
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:32:50 -0400

    Committed by GitUtil

---
+++ Banking/Savings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking {
+    class Savings : Account {
+
+        public decimal InterestRate { get; set; } = 0.12m;
+        private static decimal MinBalance { get; set; } = 200m;
+
+        public override void Withdraw(decimal Amount) {
+            if (Balance - Amount < MinBalance) {
+                throw new Exception($"Amount to withdraw must be < {Balance - MinBalance}");
+            }
+            base.Withdraw(Amount);
+        }
+
+        public decimal CalculateInterest(int Months) {
+            var interestAmount = Balance * (InterestRate / 12) * Months;
+            Deposit(interestAmount);
+            return interestAmount;
+        }
+
+        public static Savings OpenSavingsAccount(decimal OpenAmountDeposit) {
+            if (OpenAmountDeposit < MinBalance) {
+                throw new Exception($"Opening savings requires {MinBalance} deposit.");
+            }
+            var savings = new Savings();
+            savings.Deposit(OpenAmountDeposit);
+            return savings;
+        }
+
+        private Savings() { }
+        public Savings(decimal OpenAmountDeposit) : base() {
+            if (OpenAmountDeposit < MinBalance) {
+                throw new Exception($"Opening savings requires {MinBalance} deposit.");
+            }
+            Deposit(OpenAmountDeposit);
+        }
+    }
+}
